91b7690b7a7b5365cd6f82f472624411400d6ea9
Avinash Ganore <avinashganore@gmail.com>
2025-08-27 01:13:58 +0530

    Fixed validation error by fixing send-message route

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -1,12 +1,12 @@
 import dbconnect from "@/lib/dbConnect";
 import User from "@/models/User";
 import { Message } from "@/models/User";
-import { request } from "http";
 
 export async function POST(request: Request) {
   await dbconnect();
 
   const { username, content } = await request.json();
+
   try {
     const user = await User.findOne({ username });
     if (!user) {
@@ -15,14 +15,13 @@ export async function POST(request: Request) {
           success: false,
           message: "User not found",
         },
-        { 
+        {
           status: 404,
         }
       );
     }
 
-
-    // is user accepting messages
+    // is user accepting messages?
 
     if (!user.isAcceptingMessage) {
       return Response.json(
@@ -30,28 +29,29 @@ export async function POST(request: Request) {
           success: false,
           message: "User is not accepting messages",
         },
-        { 
+        {
           status: 403,
         }
       );
     }
 
-    const newMessage = {content, createdAt: new Date()}
-    user.messages.push(newMessage as Message);
-    await user.save();
+      const newMessage = { content, createdAt: new Date() };
+      
+      user.messages.push(newMessage as Message);
+      user.save();
 
     return Response.json(
-        {
-          success: true,
-          message: "Message sent successfully!",
-        },
-        { 
-          status: 200,
-        }
-      );
+      {
+        success: true,
+        message: "Message sent successfully!",
+      },
+      {
+        status: 200,
+      }
+    );
   } catch (error) {
     console.log("An unxpected error ocurred", error);
-    
+
     return Response.json(
       {
         success: false,
